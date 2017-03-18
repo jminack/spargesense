13f91b51874146769693f007bc1beaa4022f9d25
John Minack <john.minack@gmail.com>
2017-03-18 21:07:11 +1100

    wip

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 import pyaudio
 import wave
 import librosa
+from sys import byteorder
+from array import array
+from struct import pack
 
+import pyaudio
+import wave
+
+THRESHOLD = 8000
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 44100
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -9,6 +19,10 @@ CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
+
+#
+# functions defined below
+#
 
 def listen():
 	p = pyaudio.PyAudio()
@@ -19,13 +33,30 @@ def listen():
                 input=True,
                 frames_per_buffer=CHUNK)
 
-	print("* recording")
-
 	frames = []
-
-	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	num_silent = 0
+	snd_started = False
+	
+	while 1:
+		# little endian, signed short
 		data = stream.read(CHUNK)
-		frames.append(data)
+		snd_data = array('h', data)
+		if byteorder == 'big':
+			snd_data.byteswap()
+
+		silent = is_silent(snd_data)
+
+		if silent:
+			if snd_started:
+				num_silent += 1
+				if num_silent > 30:
+					break
+		else:
+			if not snd_started:
+				snd_started = True
+
+		if snd_started:
+			frames.append(data)
 
 	print("* done recording")
 
@@ -61,5 +92,106 @@ def play():
 
 	p.terminate()
 	
-listen()
-play()	
+silence_debounce = 0
+
+def is_silent(snd_data):
+	global silence_debounce
+	"Returns 'True' if below the 'silent' threshold"
+	max_data = max(snd_data)
+	#print max_data
+	if max_data > THRESHOLD:
+		silence_debounce += 1
+	else:
+		silence_debounce = 0
+	return silence_debounce <= 2 
+
+def normalize(snd_data):
+    "Average the volume out"
+    MAXIMUM = 16384
+    times = float(MAXIMUM)/max(abs(i) for i in snd_data)
+
+    r = array('h')
+    for i in snd_data:
+        r.append(int(i*times))
+    return r
+
+def trim(snd_data):
+    "Trim the blank spots at the start and end"
+    def _trim(snd_data):
+        snd_started = False
+        r = array('h')
+
+        for i in snd_data:
+            if not snd_started and abs(i)>THRESHOLD:
+                snd_started = True
+                r.append(i)
+
+            elif snd_started:
+                r.append(i)
+        return r
+
+    # Trim to the left
+    snd_data = _trim(snd_data)
+
+    # Trim to the right
+    snd_data.reverse()
+    snd_data = _trim(snd_data)
+    snd_data.reverse()
+    return snd_data
+
+def add_silence(snd_data, seconds):
+    "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
+    r = array('h', [0 for i in xrange(int(seconds*RATE))])
+    r.extend(snd_data)
+    r.extend([0 for i in xrange(int(seconds*RATE))])
+    return r
+
+def record():
+    """
+    Record a word or words from the microphone and 
+    return the data as an array of signed shorts.
+
+    Normalizes the audio, trims silence from the 
+    start and end, and pads with 0.5 seconds of 
+    blank sound to make sure VLC et al can play 
+    it without getting chopped off.
+    """
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT, channels=1, rate=RATE,
+        input=True, output=True,
+        frames_per_buffer=CHUNK_SIZE)
+
+
+    sample_width = p.get_sample_size(FORMAT)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    r = normalize(r)
+    r = trim(r)
+    r = add_silence(r, 0.5)
+    print("record() returns a sample of width ",sample_width)
+    return sample_width, r
+
+def record_to_file(path):
+    "Records from the microphone and outputs the resulting data to 'path'"
+    sample_width, data = record()
+    data = pack('<' + ('h'*len(data)), *data)
+
+    wf = wave.open(path, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(sample_width)
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
+
+	
+"""
+main function
+"""
+print "Starting"
+
+while 1:
+	listen()
+	play()
+	
